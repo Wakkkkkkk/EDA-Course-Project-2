af8690903cb81664427ee0d79189fc0a813b4022
Nathan L <nathan.lachance@gmail.com>
2021-06-06 13:06:36 -0400

    Added code to save the new plot from the code.

--- Plot6.R
+++ Plot6.R
@@ -6,20 +6,6 @@ if(!exists("LA")){
     NEI[NEI$fips=="06037",1:6]->LA
 }
 
-if(!exists("motorb")){
-    #Search for all the SCC codes corresponding to vehicles
-    SCC$SCC[grep("Vehicle", SCC$EI.Sector)] -> codes
-    #Find matches in NEI (time consuming and inefficient, but it works)
-    scclist = matches(as.character(codes),vars = baltimore$SCC)
-    motorb <- baltimore[scclist,1:6]
-    scclist = matches(as.character(codes),vars = baltimore$SCC)
-    motorl <- LA[scclist,1:6]
-}
-
-motorb %>%
-    group_by(year) %>% #Groups the data so it can be processed by summarise
-    summarise(Emissions=sum(Emissions))->motorbsum #finds the sum of emissions by year
-
 LA[LA$type=="ON-ROAD", 1:6] -> lroad
 lroad %>%
     group_by(year) %>% #Groups the data so it can be processed by summarise
@@ -30,8 +16,10 @@ road %>%
     group_by(year) %>% #Groups the data so it can be processed by summarise
     summarise(Emissions=sum(Emissions))->motorbsum #finds the sum of emissions by year
 
+png(file="plot6.png")
 #create the graph, with appropriate labels and axes
 with(motorbsum, plot(year, log(Emissions), type ="n", main = "Vehicle PM2.5 Emissions by Locality", ylim = c(0,10)))
 with(motorlsum, lines(year,log(Emissions), col ="blue"))
 with(motorbsum, lines(year,log(Emissions), col ="red"))
 legend("topright",col = c("red","blue"),lty = 1, legend = c("Baltimore", "Los Angeles"))
+dev.off()
